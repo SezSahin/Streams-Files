1f91c844b049771871233f0de27c77ff1764b656
SezSahin <sezg0006@edu.eal.dk>
2017-11-05 20:40:46 +0100

    Add project files.

---
+++ Filess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Filess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamWriter writer;
+            //writer = new StreamWriter("test");
+            //writer.WriteLine("Hello Friend! :)");
+            //writer.Close();
+
+            //StreamReader reader = new StreamReader("test");
+            //string line = reader.ReadLine();
+            //Console.WriteLine(line);
+            //reader.Close();
+
+            StreamReader read = new StreamReader("test");
+            while (read.EndOfStream == false)
+            {
+                string lines = read.ReadLine();
+                Console.WriteLine(lines);
+            }
+            read.Close();
+        }
+    }
+}
